e33aca531078160880a53afc5d06b7e23ce66ebc
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2020-06-11 19:20:27 +0900

    Update suuryou2.py

--- suuryou2.py
+++ suuryou2.py
@@ -1,21 +1,15 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-# CSVァイルを読み込んでデータフレームに格納
 df = pd.read_csv("m1-2.csv",encoding="shift-jis")
 
-# 説明変数をダミー変数に変換
 x = pd.get_dummies(df[['天気', '風']])
 
-# 目的変数：満足度
 y = df['ゴルフ'].values
 
-# 予測モデルを作成(ランダムフォレスト)
 clf = RandomForestClassifier(n_estimators=10)
 clf.fit(x, y)
 importances = clf.feature_importances_
-
-#出力
 
 out=[]
 tmp=[]
