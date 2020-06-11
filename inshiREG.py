e0d8d5fb462f1590042d496132fe33ab1ae40d48
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2020-06-11 22:36:46 +0900

    Create inshiREG.py

---
+++ inshiREG.py
@@ -0,0 +1,43 @@
+import csv
+from sklearn import linear_model
+
+f=open("rent.csv","r")
+reader=csv.reader(f)
+k=0
+name=[]
+tmp=[]
+x=[]
+y=[]
+for row in reader:
+    if k==0:
+        for j in range(len(row)):
+            if j!=0:
+                name.append(row[j])
+        k=1
+    else:
+        for i in range(len(row)):
+            if i!=0:
+                tmp.append(float(row[i]))
+            else:
+                y.append(float(row[i]))
+        x.append(tmp)
+        tmp=[]
+reg = linear_model.LinearRegression()
+reg.fit(x, y)
+a = reg.coef_
+b = reg.intercept_
+out=[]
+tmp=[]
+for i in range(len(name)):
+    tmp.append(name[i])
+    tmp.append(a[i])
+    out.append(tmp)
+    tmp=[]
+tmp.append("切片")
+tmp.append(b)
+out.append(tmp)
+tmp=[]
+tmp.append("決定係数")
+tmp.append(reg.score(x,y))
+out.append(tmp)
+print(out)
