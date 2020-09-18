6863d93a0cce83b99150cdfa89f545ebff6923b8
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2020-09-19 01:43:29 +0900

    Add files via upload

---
+++ テキストマイニング/textmining.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import LatentDirichletAllocation
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+tex=fetch_20newsgroups(remove=("headers","footers","quotes"))
+
+tf_vec=TfidfVectorizer()
+X_train_counts=tf_vec.fit_transform(tex.data)
+model=LatentDirichletAllocation(n_components=20)
+model.fit(X_train_counts)
+
+
+count_vec=CountVectorizer()
+X_train_counts=count_vec.fit_transform(tex.data)
+model2=LatentDirichletAllocation(n_components=20)
+model2.fit(X_train_counts)
+
+
+feature_names = tf_vec.get_feature_names()
+sorting = np.argsort(model.components_, axis=1)[:, ::-1]
+mglearn.tools.print_topics(topics=range(20),
+                           feature_names=np.array(feature_names),
+                           topics_per_chunk=20,
+                           sorting=sorting,n_words=10)
