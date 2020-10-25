09de7db8f009a5799c9110c42bb92bd2684d396f
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2020-10-26 01:12:37 +0900

    Update mfccpredict.py

--- mfccpredict.py
+++ mfccpredict.py
@@ -15,17 +15,17 @@ data=[]
 label=[]
 
 for i in range(9):
-    path="Asami/1-"+str(i+1)+".mp3"
+    path="フォルダ名"+str(i+1)+"拡張子名"
     a,sr=librosa.load(path)
-    a=a[:100000]
+    a=a[:100000]#サイズ合わせ
     y=librosa.feature.mfcc(y=a,sr=sr)
     data.append(y)
     label.append(0)
 
 for i in range(10):
-    path="Yuuki/2-"+str(i+1)+".wav"
+    path="フォルダ名"+str(i+1)+"拡張子名"
     a,sr=librosa.load(path)
-    a=a[:100000]
+    a=a[:100000]#サイズ合わせ
     y=librosa.feature.mfcc(y=a,sr=sr)
     data.append(y)
     label.append(1)
@@ -33,21 +33,21 @@ for i in range(len(data)):
     data[i]=sum(data[i])/len(data[i])
 
 #データの分割
-train_x, test_x, train_y, test_y = train_test_split(data, label, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0.3)
 
 #学習
 clf=clf=MLPClassifier(hidden_layer_sizes=(500,500,250))
-clf.fit(train_x, train_y)
+clf.fit(x_train, y_train)
 
 #予測
-y_pred=clf.predict(test_x)
+y_pred=clf.predict(x_test)
 
 #精度の検証
 print("Accuracy")
-print(accuracy_score(test_y,y_pred))
+print(accuracy_score(y_test,y_pred))
 print("Precision")
-print(precision_score(test_y, y_pred,average="macro"))
+print(precision_score(y_test, y_pred,average="macro"))
 print("Recall")
-print(recall_score(test_y, y_pred,average="macro"))
+print(recall_score(y_test, y_pred,average="macro"))
 print("F1-score")
-print(f1_score(test_y, y_pred,average="macro"))
+print(f1_score(y_test, y_pred,average="macro"))
