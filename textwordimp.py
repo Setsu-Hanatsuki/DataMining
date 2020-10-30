3b552993b733cb2dc30ede63afb4315584b7f6e6
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2020-10-30 13:46:22 +0900

    Update textwordimp.py

--- textwordimp.py
+++ textwordimp.py
@@ -2,10 +2,6 @@
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 import numpy as np
 import csv
 
@@ -19,9 +15,6 @@ x=tivec.fit_transform(text.data)
 x=x.toarray()
 y=text.target
 x=preprocessing.minmax_scale(x)
-
-#データの分割
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 #モデルの生成と学習
 clf=DecisionTreeClassifier()
