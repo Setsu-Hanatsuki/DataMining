d6de4db26c417dec5234d9f4b3bb9af5f67cf22a
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2020-10-31 18:04:48 +0900

    Update textwordimp.py

--- textwordimp.py
+++ textwordimp.py
@@ -2,8 +2,13 @@
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 import numpy as np
 import csv
+import pandas as pd
 
 #データのロード
 category=["talk.religion.misc","soc.religion.christian","sci.space","comp.graphics"]
@@ -16,6 +21,9 @@ x=x.toarray()
 y=text.target
 x=preprocessing.minmax_scale(x)
 
+#データの分割
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
 #モデルの生成と学習
 clf=DecisionTreeClassifier()
 clf.fit(x, y)
@@ -26,9 +34,14 @@ imp=clf.feature_importances_
 total=[]
 total.append(name)
 total.append(imp)
-
+total=np.array(total).T
+df=pd.DataFrame(total)
+df.columns=["word","importance"]
+print(df.sort_values(by="importance",ascending=False))
+"""
 #保存
 fb = open("importance_word.csv", 'w',encoding='utf-8')
 writer = csv.writer(fb, lineterminator='\n')
 writer.writerows(total)
 fb.close()
+"""
