7e47fbc9fb9a6bc217f009255aaa2ff30d9fc1a8
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2020-11-05 21:39:24 +0900

    Update imagefilter.py

--- imagefilter.py
+++ imagefilter.py
@@ -1,82 +1,36 @@
 from PIL import Image
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 image=np.array(Image.open("noise.jpg"))
 
-for i in range(len(image)):
-    for j in range(len(image[i])):
-        tmp1=[]
-        tmp2=[]
-        tmp3=[]
-        try:
-            tmp1.append(image[i-1][j-1][0])
-            tmp2.append(image[i-1][j-1][1])
-            tmp3.append(image[i-1][j-1][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i][j-1][0])
-            tmp2.append(image[i][j-1][1])
-            tmp3.append(image[i][j-1][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i+1][j-1][0])
-            tmp2.append(image[i+1][j-1][1])
-            tmp3.append(image[i+1][j-1][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i-1][j][0])
-            tmp2.append(image[i-1][j][1])
-            tmp3.append(image[i-1][j][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i][j][0])
-            tmp2.append(image[i][j][1])
-            tmp3.append(image[i][j][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i+1][j][0])
-            tmp2.append(image[i+1][j][1])
-            tmp3.append(image[i+1][j][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i-1][j+1][0])
-            tmp2.append(image[i-1][j+1][1])
-            tmp3.append(image[i-1][j+1][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i][j+1][0])
-            tmp2.append(image[i][j+1][1])
-            tmp3.append(image[i][j+1][2])
-            
-        except:
-            a=1
-        try:
-            tmp1.append(image[i+1][j+1][0])
-            tmp2.append(image[i+1][j+1][1])
-            tmp3.append(image[i+1][j+1][2])
-            
-        except:
-            a=1
-        tmp1=np.array(tmp1)
-        tmp2=np.array(tmp2)
-        tmp3=np.array(tmp3)
-        image[i][j][0]=np.median(tmp1)
-        image[i][j][1]=np.median(tmp2)
-        image[i][j][2]=np.median(tmp3)
-plt.imshow(image)
+#plt.imshow(image)
+#plt.show()
+
+#輪郭強調
+kernel=np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]],np.float32)
+dst=cv2.filter2D(image,-1,kernel)
+dst=image+dst
+#plt.imshow(dst)
+#plt.show()
+
+#コントラスト強調
+alpha=1.0
+beta=30.0
+con=image*alpha+beta
+con=np.clip(con,0,255).astype(np.uint8)
+#plt.imshow(con)
+#plt.show()
+
+#ノイズフィルタリング(非線形)
+fil=cv2.medianBlur(image,9)
+#plt.imshow(fil)
+#plt.show()
+
+con2=fil*alpha+beta
+con2=np.clip(con,0,255).astype(np.uint8)
+con2=cv2.medianBlur(image,5)
+plt.imshow(con2)
 plt.show()
